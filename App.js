9ba8a8aa22f879eff23863c5ca6c87762fc388fc
Maksym Kazhaiev <mkazhaev746@gmail.com>
2022-01-28 11:43:38 +0100

    First commit

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// View -> UIView
 export default function App() {
+  console.log("App executed");
+
+  let x;
+  x.toString();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
