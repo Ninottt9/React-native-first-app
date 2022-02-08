03241ade0b7ab40935acd94af70053a7169d00f2
Maksym Kazhaiev <mkazhaev746@gmail.com>
2022-02-09 01:30:19 +0200

    Align items

--- App.js
+++ App.js
@@ -10,20 +10,24 @@ export default function App() {
     <View style={{
       backgroundColor: "#fff",
       flex: 1,
-      flexDirection: "row",
-    }}>
+      flexDirection: "row", //horizontal main axis
+      justifyContent: "center", //main axis
+      alignItems: "center", //secondary axis
+    }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
           width: 100,
-          height: 100,
+          height: 300,
+          alignSelf: "flex-start"
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 100,
+          height: 200,
         }}
       />
       <View
