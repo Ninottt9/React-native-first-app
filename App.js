a3b35d0f1fd9aaef1d2e771ae836650774c538aa
Maksym Kazhaiev <mkazhaev746@gmail.com>
2022-02-08 23:14:54 +0200

    Button with alert created

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableNativeFeedback, Button, Alert } from 'react-native';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 // View -> UIView
 export default function App() {
-  let x = 1;
-  console.log("App executed!");
 
   return (
-    <View style={styles.container}>
-      <Text>What??</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, containerStyle]}>
+      <Button
+        // backgroundColor="orange"
+        color="orange"
+        title="Click me"
+        onPress={() => Alert.alert("Alert!", "Sergeii!!", [
+          { text: "Yes", onPress: () => console.log("Yes") },
+          { text: "No", onPress: () => console.log("No") },
+        ])
+        }
+      />
+    </SafeAreaView >
   );
 }
+
+const containerStyle = { backgroundColor: "orange" };
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
