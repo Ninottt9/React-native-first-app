a2ee93a41e2434c8b12de993dfe8ccf1d20e570a
Maksym Kazhaiev <mkazhaev746@gmail.com>
2022-02-09 01:11:00 +0200

    Auto orientaiton added

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableNativeFeedback, Button, Alert, Platform, StatusBar } from 'react-native';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import {
+  StyleSheet, Text, View, Image, SafeAreaView, Dimensions, Button, Alert, Platform, StatusBar
+} from 'react-native';
+
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+
+
 
 // View -> UIView
 export default function App() {
 
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-        // backgroundColor="orange"
-        color="orange"
-        title="Click me"
-        onPress={() => Alert.alert("Alert!", "Sergeii!!", [
-          { text: "Yes", onPress: () => console.log("Yes") },
-          { text: "No", onPress: () => console.log("No") },
-        ])
-        }
-      />
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '100%',
+        height: landscape ? "100" : "30",
+      }}></View>
+      <Text>HELLO SEREGA, how are you?</Text>
+      {/* <Button
+        title="Mom, press"
+        onPress={() => Alert.alert("Wow, you clicked??", "You happy?", [
+          { text: "Yes" },
+          { text: "Yes" },
+        ])}
+      /> */}
     </SafeAreaView >
   );
 }
-
-const containerStyle = { backgroundColor: "orange" };
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    backgroundColor: "white",
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
